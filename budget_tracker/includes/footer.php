8d036eb7b856adfb66df11067694ab6ec4e2eabf
chogocorban <156567997+chogocorban@users.noreply.github.com>
2025-07-01 07:56:18 +0300

    Removed the different version

--- budget_tracker/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-
-    <footer class="main-footer">
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Budget Tracker. All rights reserved.</p>
-        </div>
-    </footer>
-</body>
-</html>
--- budget_tracker/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require 'includes/config.php';
-require 'includes/auth_functions.php';
-
-logoutUser();
-header("Location: login.php");
-exit;
-?>
